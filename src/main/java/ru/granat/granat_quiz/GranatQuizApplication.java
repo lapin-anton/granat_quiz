dfb3f787396b8d8601d0ce7f51dcf77b429d02ed
anton-lapin <lapin_antoine@mail.ru>
2022-06-16 22:28:46 +0300

    initial

---
+++ src/main/java/ru/granat/granat_quiz/GranatQuizApplication.java
@@ -0,0 +1,13 @@
+package ru.granat.granat_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranatQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranatQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/granat/granat_quiz/GranatQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.granat.granat_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranatQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
